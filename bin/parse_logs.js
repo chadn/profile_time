4d5e5b57462eb34bd884c7f43852a5af32496e92
Chad Norwood <chad@chadnorwood.com>
2013-03-29 11:37:45 -0500

    Support options, url stats

--- bin/parse_logs.js
+++ bin/parse_logs.js
@@ -13,14 +13,46 @@
  *  heroku logs -n 1500  | ./parse_logs.js
  */
 
-var fs = require('fs');
-var lazy = require('lazy');
+var lazy         = require('lazy');
+var program      = require('commander');
+var fs           = require('fs');
 var profile_time = require('../lib/profile_time');
-var myProfiler = profile_time.Profiler();
+var myProfiler;
 
+function help() {
+  console.log('  Description:');
+  console.log('');
+  console.log('  Displays stats based on profile_time log lines, including total, percent, and average times.');
+  console.log('  By default, it looks only at the json data string, including any begin/end key data.');
+  console.log('  If url option is passed (-u), the total time for each url is processed instead.');
+  console.log('');
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ cat file.log | parse_logs.js');
+  console.log('    $ cat file.log | parse_logs.js -u 2');
+  console.log('    $ parse_logs.js -l file.log');
+  console.log('    $ parse_logs.js -u 1 -l file.log');
+  console.log('');
+}
 function init() {
-	if (process.argv.length > 2) {
-		readLazy(fs.createReadStream(process.argv[2]));
+	program
+		.version('0.0.2')
+		.option('-l, --logfile [file.log]', 'parse file.log, not stdin. Looks to stdin if no logfile')
+		.option('-u, --url [n]', 'Display stats from url instead of from json key data.\n'
+		   + 'Group urls by at most depth n, where depth is number of slashes: /1/2/3')
+		//.option('-t, --top [n]', 'only show stats for the top [n] values')
+		//.option('-v, --verbose', 'be verbose in output')
+		.on('--help', help)
+		.parse(process.argv);
+
+	myProfiler   = profile_time.Profiler({ 
+		//topValues : program.top,
+		urls : program.url
+	});
+	//process.stdout.write(program.helpInformation());
+	
+	if (program.logfile) {
+		readLazy( fs.createReadStream(program.logfile) );
 	} else {
 		process.stdin.resume();
 		process.stdin.setEncoding('utf8');
@@ -58,6 +90,7 @@ function processLine(line) {
 		t.total.linesProd++;
 	}
 	*/
+	//console.log('processLine DONE:', [p]);
 	myProfiler.addLineToTotal(p);
 }
 
